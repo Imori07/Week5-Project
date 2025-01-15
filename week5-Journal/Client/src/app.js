d887d13d97fee47c53f633767df585cd6ee562c2
Craig Clayton <craig__clayton@outlook.com>
2025-01-15 15:03:57 +0000

    default image added just incase user leaves image input empty

--- week5-Journal/Client/src/app.js
+++ week5-Journal/Client/src/app.js
@@ -106,15 +106,15 @@
 
 // // postReviews();
 
-const form = document.getElementById("review_form");
-const commentContainer = document.getElementById("comment-container");
+const form = document.getElementById('review_form');
+const commentContainer = document.getElementById('comment-container');
 
-const BASE_URL = "https://week5-project-09nc.onrender.com";
+const BASE_URL = 'https://week5-project-09nc.onrender.com';
 
 const fetchReviews = async () => {
   const response = await fetch(`${BASE_URL}/reviews`);
   const data = await response.json();
-  commentContainer.innerHTML = "";
+  commentContainer.innerHTML = '';
   data
     .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
     .forEach((review) => createReview(review));
@@ -124,10 +124,10 @@ fetchReviews();
 
 const postReviews = async (e) => {
   e.preventDefault();
-  const reviewDescription = document.querySelector("#review_description").value;
+  const reviewDescription = document.querySelector('#review_description').value;
 
   if (reviewDescription.length < 15) {
-    alert("Must be more than 15 characters long");
+    alert('Must be more than 15 characters long');
     return;
   }
 
@@ -135,9 +135,9 @@ const postReviews = async (e) => {
   const formEntries = Object.fromEntries(formData);
 
   const response = await fetch(`${BASE_URL}/reviews`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(formEntries),
   });
@@ -149,28 +149,28 @@ const postReviews = async (e) => {
       console.log("couldn't fetch reviews");
     }
   } catch (error) {
-    console.log("Error:", error);
+    console.log('Error:', error);
   }
 };
 
-form.addEventListener("submit", postReviews);
+form.addEventListener('submit', postReviews);
 
 async function handleDeleteReview() {
   const deleteConfirmation = confirm(
-    "Are you sure you want to delete this review?"
+    'Are you sure you want to delete this review?'
   );
 
   if (!deleteConfirmation) {
     return;
   }
 
-  const commentDiv = this.closest("div");
+  const commentDiv = this.closest('div');
   const id = commentDiv.dataset.id;
   try {
     const response = await fetch(`${BASE_URL}/reviews`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ id }),
     });
@@ -179,38 +179,38 @@ async function handleDeleteReview() {
       await fetchReviews();
     }
   } catch (error) {
-    console.error("Error during delete request:", error);
+    console.error('Error during delete request:', error);
   }
 }
 
 function createReview(review) {
-  const reviewDiv = document.createElement("div");
-  reviewDiv.className = "review";
+  const reviewDiv = document.createElement('div');
+  reviewDiv.className = 'review';
   reviewDiv.dataset.id = review.id;
 
-  const username = createParagraph("username", review.name);
+  const username = createParagraph('username', review.name);
 
   const description = createParagraph(
-    "review-description",
+    'review-description',
     review.review_description
   );
 
-  const image = createImage("review-image", review.image_url);
+  const image = createImage('review-image', review.image_url);
 
-  const formattedDate = new Intl.DateTimeFormat("en-GB").format(
+  const formattedDate = new Intl.DateTimeFormat('en-GB').format(
     new Date(review.created_at)
   );
 
-  const reviewDate = createParagraph("date", formattedDate);
+  const reviewDate = createParagraph('date', formattedDate);
 
-  const rating = createParagraph("rating", `${"⭐".repeat(review.rating)}`);
+  const rating = createParagraph('rating', `${'⭐'.repeat(review.rating)}`);
 
   const deleteButton = createButton(
-    "fa-solid fa-xmark delete-btn",
-    "delete review"
+    'fa-solid fa-xmark delete-btn',
+    'delete review'
   );
 
-  deleteButton.addEventListener("click", handleDeleteReview);
+  deleteButton.addEventListener('click', handleDeleteReview);
 
   reviewDiv.appendChild(username);
   reviewDiv.appendChild(image);
@@ -226,21 +226,23 @@ function createReview(review) {
 }
 
 function createParagraph(className, text) {
-  const p = document.createElement("p");
+  const p = document.createElement('p');
   p.className = className;
   p.textContent = text;
   return p;
 }
 
 function createImage(className, url) {
-  const image = document.createElement("img");
+  const image = document.createElement('img');
   image.className = className;
-  image.src = url;
+  image.src =
+    url ||
+    'https://cdn.discordapp.com/avatars/318351745711472641/d9ef7d1f0b38d087f5e2e31c77a43394.webp?size=160';
   return image;
 }
 
 function createButton(className, text) {
-  const button = document.createElement("i");
+  const button = document.createElement('i');
   button.className = className;
   button.title = text;
   return button;
